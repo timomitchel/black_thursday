f3221eab409c612b3a57bdc922f29d67a663b901
Miguel Silva <msilva0897@gmail.com>
2017-09-16 13:36:53 -0600

    math mod updated

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -17,35 +17,6 @@ class SalesAnalyst
      (items.to_f / merchants.to_f).round(2)
    end
 
-   def average_items_per_merchant_standard_deviation
-     mean = engine.merchants.all.map do |merchant|
-       (merchant.items.length - average_items_per_merchant)**2
-     end
-     Math.sqrt(mean.sum / (engine.merchants.all.count - 1)).round(2)
-   end
-
-   def merchants_with_high_item_count
-     total = one_standard_deviation
-     high_sellers = engine.merchants.all.select do |merchant|
-       merchant.items.length >= total
-     end
-     high_sellers
-   end
-
-   def average_item_price_for_merchant(merchant_id)
-     items = engine.items.find_all_by_merchant_id(merchant_id)
-     average = 0.00
-     items.each {|item| average += item.unit_price}
-     (average / items.count).round(2)
-   end
-
-   def average_average_price_per_merchant
-     average = engine.merchants.all.map do |merchant|
-       average_item_price_for_merchant(merchant.id)
-     end
-     (average.sum / average.length).round(2)
-   end
-
    def golden_items
      average = average_average_price_per_merchant
      items = engine.items.all
@@ -54,17 +25,6 @@ class SalesAnalyst
      total = average + (stnd_dev * 2).round(2)
      golden_items = items.select {|item| item.unit_price >= total}
      golden_items
-   end
-
-   def average_invoices_per_merchant
-     (engine.invoices.all.count.to_f / engine.merchants.all.count.to_f).round(2)
-   end
-
-   def average_invoices_per_merchant_standard_deviation
-     mean = engine.merchants.all.map do |merchant|
-       (merchant.invoices.length - average_invoices_per_merchant)**2
-     end
-     Math.sqrt(mean.sum / (engine.merchants.all.count - 1)).round(2)
    end
 
    def top_merchants_by_invoice_count
