bf329db335e8c12c01cf2231219c97e01ddc7987
Miguel Silva <msilva0897@gmail.com>
2017-09-17 17:12:39 -0600

    refactored sales analyst

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,9 +1,11 @@
 require_relative "sales_engine"
 require_relative "math"
+require_relative "merchant_analyst"
 require "pry"
 
 class SalesAnalyst
   include Math
+  include MerchantAnalyst
 
    attr_reader :engine
 
@@ -65,80 +67,5 @@ class SalesAnalyst
        invoice.status == input
      end
      ((all.count.to_f / engine.invoices.all.count) * 100).round(2)
-   end
-
-   def total_revenue_by_date(date)
-     all = engine.invoices.all.select do |invoice|
-       invoice.created_at == date
-     end
-    all.reduce(0) {|sum, invoice| sum += invoice.total}
-   end
-
-   def top_revenue_earners(x = 20)
-    merchants = engine.merchants.all.select do |merchant|
-       merchant.invoices.count > 0
-    end
-    top = merchants.map{|merchant| [merchant, revenues(merchant.id)]}
-    top = top.sort_by {|merchant| merchant[1]}.reverse
-    all = []
-    x.times {|index| all << top[index][0]}
-    all
-   end
-
-   def revenues(id)
-     revenue = engine.merchants.find_by_id(id).revenue.to_f.round(2)
-   end
-
-   def merchants_with_pending_invoices
-     total = engine.merchants.all.select do |merchant|
-       merchant.has_pending_invoices?
-     end
-   end
-
-   def merchants_with_only_one_item
-     one_item
-   end
-
-   def merchants_with_only_one_item_registered_in_month(month)
-    one_item.select do |merchant|
-       months(merchant.created_at.month) == month
-     end
-   end
-
-   def months(month)
-    all_months =  ["January", "February","March","April",
-      "May","June","July","August",
-      "September","October","November","December"]
-      all_months[month - 1]
-   end
-
-   def one_item
-     singles = engine.merchants.all.find_all do |merchant|
-       merchant.has_one_item?
-     end
-   end
-
-   def revenue_by_merchant(merchant_id)
-     engine.merchants.find_by_id(merchant_id).revenue
-   end
-
-   def most_sold_item_for_merchant(merchant_id)
-     invoices = engine.invoices.find_all_by_merchant_id(merchant_id)
-     all = invoices.select {|invoice| invoice.is_paid_in_full?}
-     total = all.map {|invoice| invoice.invoice_items}
-     turn_to_items(top_items(total))
-   end
-
-   def top_items(total)
-     max = total.flatten.max_by {|invoice_item| invoice_item.quantity}
-     top_items = total.flatten.select do |invoice_item|
-       invoice_item.quantity == max.quantity
-     end
-   end
-
-   def turn_to_items(top_item)
-     top_item.flatten.map do |invoice_item|
-        engine.items.find_by_id(invoice_item.item_id)
-     end
    end
 end
