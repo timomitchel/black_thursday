8d031bb065a0971e9de889a8f41fde5e99830009
Miguel Silva <msilva0897@gmail.com>
2017-09-17 22:24:33 -0600

    4 more test in merchant repo

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -33,45 +33,29 @@ class MerchantRepositoryTest < Minitest::Test
     assert_equal "Shopin1901", repository.find_by_id(12334105).name
     assert_nil nil, repository.find_by_id(12)
   end
-  #
-  # def test_all_if_returns_all_instances
-  #   assert_equal 12, repository.all.count
-  # end
-  #
-  # def test_find_all_with_description_returns_description
-  #   assert_equal Item, repository.find_all_with_description("Ugly necklaces for everyone").first.class
-  #   assert_equal "Christmas necklaces", repository.find_all_with_description("Ugly necklaces for everyone").first.name
-  # end
-  #
-  # def test_find_all_by_price_returns_item
-  #   assert_equal Item, repository.find_all_by_price(420.00).first.class
-  #   assert_equal "Christmas skirts", repository.find_all_by_price(420.00).first.name
-  # end
-  #
-  # def test_find_all_by_price_range_return_array
-  #   assert_equal Item, repository.find_all_by_price_in_range((130..140)).first.class
-  #   assert_equal 2, repository.find_all_by_price_in_range((130..140)).count
-  # end
-  #
-  # def test_find_all_by_merchant_id_return_item
-  #   assert_equal Item, repository.find_all_by_merchant_id(12341234).first.class
-  #   assert_equal 7, repository.find_all_by_merchant_id(12341234).count
-  # end
-  #
-  # def test_if_parent_method_works
-  #   se = SalesEngine.new({:items => "./test/fixtures/truncated_items.csv",
-  #                        :merchants => "./test/fixtures/truncated_merchants.csv",
-  #                        :invoices => "./test/fixtures/truncated_invoices.csv",
-  #                        :invoice_items => "./test/fixtures/truncated_invoice_items.csv",
-  #                        :transactions => "./test/fixtures/truncated_customers.csv",
-  #                        :customers =>  "./test/fixtures/truncated_transactions.csv"})
-  #   rep = ItemRepository.new("./test/fixtures/truncated_items.csv" , se)
-  #
-  #   assert_equal se, rep.parent
-  # end
-  #
-  # def test_find_by_merchant_id_works
-  #   assert_equal Merchant, rep.find_by_merchant_id(12334105).class
-  # end
+
+  def test_all_if_returns_all_instances
+    assert_equal 59, repository.all.count
+  end
+
+  def test_find_all_by_name_returns_array
+    assert_equal Merchant, repository.find_all_by_name("S").first.class
+    assert_equal "Shopin1901", repository.find_all_by_name("S").first.name
+    assert_equal 36, repository.find_all_by_name("S").count
+    assert_nil nil, repository.find_all_by_name("Z")
+  end
+
+  def test_find_customer_by_merchant_id_return_customer
+    assert_equal Customer, repository.find_customer_by_merchant_id(12334112).first.class
+    assert_equal 5, repository.find_customer_by_merchant_id(12334112).first.id
+  end
+
+  def test_find_items_by_merchant_return_items
+
+  end
+
+  def test_
+
+  end
 
 end
