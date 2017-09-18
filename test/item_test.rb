b72f559aadcf855cf69425108a66d9ecbf32cc6a
Miguel Silva <msilva0897@gmail.com>
2017-09-18 10:25:45 -0600

    new item test

---
+++ test/item_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require_relative "../lib/item"
+require_relative "../lib/item_repository"
+require_relative "../lib/sales_engine"
+
+class ItemTest < Minitest::Test
+attr_reader :item
+
+  def setup
+    @se = SalesEngine.new({:items => "./test/fixtures/truncated_items.csv",
+                         :merchants => "./test/fixtures/truncated_merchants.csv",
+                         :invoices => "./test/fixtures/truncated_invoices.csv",
+                         :invoice_items => "./test/fixtures/truncated_invoice_items.csv",
+                         :transactions => "./test/fixtures/truncated_customers.csv",
+                         :customers =>  "./test/fixtures/truncated_transactions.csv"})
+    @repository = ItemRepository.new("./test/fixtures/truncated_items.csv" , @se)
+    @item = Item.new({id: "123456789", name: "Christmas Sweaters", description: "Ugly sweaters for everyone", unit_price: "12111", merchant_id: "12341234", created_at: "2010-12-10", updated_at: "2011-12-04"}, @repository)
+  end
+
+  def test_if_class_exist
+    assert_instance_of Item, item
+  end
+
+  def test_if_id_returns_item_id
+    actual = Item.new({id: "223456789", name: "Christmas Sweaters", description: "Ugly sweaters for everyone", unit_price: "12111", merchant_id: "12341234", created_at: "2010-12-10", updated_at: "2011-12-04"}, @repository)
+
+    assert_equal 123456789, item.id
+    assert_nil nil, actual
+  end
+  
+end
