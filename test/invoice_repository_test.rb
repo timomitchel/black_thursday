bc1f0eab7919f814ff0412c581c54818763a9a0f
Miguel Silva <msilva0897@gmail.com>
2017-09-18 13:37:31 -0600

    working invoice repository test

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -3,6 +3,7 @@ require './lib/sales_engine'
 require 'minitest/autorun'
 
 class InvoiceRepositoryTest < Minitest::Test
+  attr_reader :repository
 
   def setup
     se = SalesEngine.new({:items => "./test/fixtures/truncated_items.csv",
@@ -11,11 +12,11 @@ class InvoiceRepositoryTest < Minitest::Test
                          :invoice_items => "./test/fixtures/truncated_invoice_items.csv",
                          :transactions => "./test/fixtures/truncated_customers.csv",
                          :customers =>  "./test/fixtures/truncated_transactions.csv"})
-    @repository = InvoiceRepository.new("./test/fixtures/truncated_merchants.csv" , se)
+    @repository = InvoiceRepository.new("./test/fixtures/truncated_invoices.csv" , se)
   end
 
   def test_csv_loads_csv_file
-    assert_equal CSV, @repository.csv_load("./test/fixtures/truncated_invoices.csv").class
+    assert_equal CSV, repository.csv_load("./test/fixtures/truncated_invoices.csv").class
   end
 
 end
