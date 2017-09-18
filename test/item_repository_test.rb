47d79316c3432764e24026cdb32e322d50c254a0
Miguel Silva <msilva0897@gmail.com>
2017-09-17 21:55:37 -0600

    tested every method in item repo

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -1,10 +1,15 @@
 require './lib/item_repository'
 require 'minitest/autorun'
+require './lib/sales_engine'
 
 class ItemRepositoryTest < Minitest::Test
   attr_reader :repository
   def setup
     @repository = ItemRepository.new("./test/fixtures/truncated_items.csv" , self)
+  end
+
+  def test_csv_loads_csv_file
+    assert_equal CSV, repository.csv_load("./test/fixtures/truncated_items.csv").class
   end
 
   def test_if_class_item_repository_exist
@@ -34,5 +39,39 @@ class ItemRepositoryTest < Minitest::Test
   def test_find_all_by_price_returns_item
     assert_equal Item, repository.find_all_by_price(420.00).first.class
     assert_equal "Christmas skirts", repository.find_all_by_price(420.00).first.name
+  end
+
+  def test_find_all_by_price_range_return_array
+    assert_equal Item, repository.find_all_by_price_in_range((130..140)).first.class
+    assert_equal 2, repository.find_all_by_price_in_range((130..140)).count
+  end
+
+  def test_find_all_by_merchant_id_return_item
+    assert_equal Item, repository.find_all_by_merchant_id(12341234).first.class
+    assert_equal 7, repository.find_all_by_merchant_id(12341234).count
+  end
+
+  def test_if_parent_method_works
+    se = SalesEngine.new({:items => "./test/fixtures/truncated_items.csv",
+                         :merchants => "./test/fixtures/truncated_merchants.csv",
+                         :invoices => "./test/fixtures/truncated_invoices.csv",
+                         :invoice_items => "./test/fixtures/truncated_invoice_items.csv",
+                         :transactions => "./test/fixtures/truncated_customers.csv",
+                         :customers =>  "./test/fixtures/truncated_transactions.csv"})
+    rep = ItemRepository.new("./test/fixtures/truncated_items.csv" , se)
+
+    assert_equal se, rep.parent
+  end
+
+  def test_find_by_merchant_id_works
+    se = SalesEngine.new({:items => "./test/fixtures/truncated_items.csv",
+                         :merchants => "./test/fixtures/truncated_merchants.csv",
+                         :invoices => "./test/fixtures/truncated_invoices.csv",
+                         :invoice_items => "./test/fixtures/truncated_invoice_items.csv",
+                         :transactions => "./test/fixtures/truncated_customers.csv",
+                         :customers =>  "./test/fixtures/truncated_transactions.csv"})
+    rep = ItemRepository.new("./test/fixtures/truncated_items.csv" , se)
+
+    assert_equal Merchant, rep.find_by_merchant_id(12334105).class
   end
 end
